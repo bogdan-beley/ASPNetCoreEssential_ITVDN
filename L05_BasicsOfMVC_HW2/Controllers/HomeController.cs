0f59438128a1420a8f9b49bc7d683d3eeb4fd133
Bogdan Beley <bogdan.beley@outlook.com>
2020-10-11 12:29:26 +0300

    L05_BasicsOfMVC_HW2

---
+++ L05_BasicsOfMVC_HW2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L05_BasicsOfMVC_HW2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home page";
+            return View();
+        }
+    }
+}
