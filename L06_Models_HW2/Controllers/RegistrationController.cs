1b8da291ed8d20ae12c5d99a1e66cf717efd7953
Bogdan Beley <bogdan.beley@outlook.com>
2020-10-12 15:29:35 +0300

    L06_Models_HW2

---
+++ L06_Models_HW2/Controllers/RegistrationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using L06_Models_HW2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L06_Models_HW2.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.Age >= 14)
+                    return Content($"Welcome, {user.FName} {user.LName}!");
+                else
+                    throw new Exception("You must be at least 14 years old to access!");
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ L06_Models_HW2/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L06_Models_HW2.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required, MinLength(3), MaxLength(20)]
+        public string FName { get; set; }
+
+        [Required, MinLength(3), MaxLength(20)]
+        public string LName { get; set; }
+
+        [Required, Range(1, 100)]
+        public int Age { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
