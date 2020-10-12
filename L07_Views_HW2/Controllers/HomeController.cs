491684f186bff37f7dbeda1251c2cf493883e973
Bogdan Beley <bogdan.beley@outlook.com>
2020-10-12 20:39:12 +0300

    L07_Views_HW2

---
+++ L07_Views_HW2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using L07_Views_HW2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L07_Views_HW2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Game> games = new List<Game>()
+            {
+                new Game {Id = 1, Name = "Legacy of Kain: Soul Reaver", Price = 20},
+                new Game {Id = 2, Name = "Carmageddon", Price = 15},
+                new Game {Id = 3, Name = "Max Payne", Price = 18},
+            };
+            
+            return View(games);
+        }
+    }
+}
---
+++ L07_Views_HW2/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L07_Views_HW2.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
