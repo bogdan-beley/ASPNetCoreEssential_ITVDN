cd80437b6e6de82adcfa67b597b997413ff89a9a
Bogdan Beley <bogdan.beley@outlook.com>
2020-10-14 09:28:58 +0300

    L08_Controllers_HW2

---
+++ L08_Controllers_HW2/Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L08_Controllers_HW2.Controllers
+{
+    public class FilesController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FilesController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public VirtualFileResult GetExampleFile()
+        {
+            string filePath = Path.Combine("~/Files", "example.txt");
+
+            return File(filePath, "Application/txt", "example.txt");
+        }
+    }
+}
