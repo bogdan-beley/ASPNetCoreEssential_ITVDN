4d46e7b65a51e003cac030e2c99e3359404dd0a0
Bogdan Beley <bogdan.beley@outlook.com>
2020-10-14 08:57:14 +0300

    L08_Controllers_HW1

---
+++ L08_Controllers_HW1/Controllers/FileController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using L08_Controllers_HW1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.AspNetCore.Hosting;
+
+namespace L08_Controllers_HW1.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(FileModel fileModel)
+        {
+            string fileName = fileModel.FileName + ".txt";
+            string fileType = "Application/txt";
+            string contentRootPath = _webHostEnvironment.ContentRootPath;
+            string filePath = Path.Combine(contentRootPath, "Files/" + fileName);
+
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine(fileModel.Fname + " " + fileModel.Lname);
+            }
+
+            return PhysicalFile(filePath, fileType, fileName);
+        }
+    }
+}
---
+++ L08_Controllers_HW1/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L08_Controllers_HW1.Models
+{
+    public class FileModel
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string FileName { get; set; }
+    }
+}
